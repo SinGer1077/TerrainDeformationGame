aaf580c0930181803e38793913aabe53884f4cff
Singer <SinGer1077@github.com>
2021-06-13 00:50:02 +0500

    13.06 bots fire

---
+++ Quickdesert/Assets/Scripts/BotsBehaviour/BulletBOt.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBOt : MonoBehaviour
+{
+    public Rigidbody rb;
+    public GameObject explosion;
+    public LayerMask whatIsEnemies;
+    public LayerMask whatIsTerrain;
+
+    [Range(0f, 1f)]
+    public float bounciness;
+    public bool useGravity;
+
+    public int explosionDamage;
+    public float explosionRange;
+    public float explosionForce;
+    public int maxCollisions;
+    public float maxLifetime;
+    public bool explodeOnTouch = true;
+
+    int collisions;
+    PhysicMaterial physics_mat;
+    bool alreadyExpload = false;
+
+    private void Start()
+    {
+        Setup();
+    }
+
+    private void Update()
+    {
+        //if (collisions > maxCollisions) 
+        //    Explode();
+
+        maxLifetime -= Time.deltaTime;
+        if (maxLifetime <= 0)
+            Explode();
+    }
+
+    private void Explode()
+    {
+        if (alreadyExpload == false)
+        {
+            if (explosion != null)
+            {
+                Instantiate(explosion, transform.position, Quaternion.identity);
+                alreadyExpload = true;
+            }
+
+            Collider[] blocks = Physics.OverlapSphere(transform.position, explosionRange, whatIsTerrain);
+            //Debug.Log("При первом выстреле получили блоков " + blocks.Length);
+            if (blocks.Length < 50)
+            {
+                List<Collider> listBlocks = new List<Collider>(blocks.Length);
+                listBlocks.AddRange(blocks);
+                for (int i = 0; i < blocks.Length; i++)
+                {
+
+                    blocks[i].GetComponent<ShootingTerrain>().TakeDamage(transform.position, explosionRange, whatIsTerrain, explosionDamage);//, listBlock
+                }
+            }
+            //Collider[] enemies = Physics.OverlapSphere(transform.position, explosionRange, whatIsEnemies);
+            //for (int i = 0; i < enemies.Length; i++)
+            //{
+            //    if (enemies[i].tag == "BotComponent")
+            //    {
+            //        enemies[i].gameObject.GetComponentInParent<BotGameplay>().TakeDamage(explosionDamage);
+            //    }
+            //    //enemies[i].gameObject.GetComponent<BotGameplay>().TakeDamage(explosionDamage);
+            //}
+            // Collider[] enemies = Physics.OverlapSphere(transform.position, explosionRange, whatIsEnemies);
+            Invoke("Delay", 0.05f);
+        }
+    }
+
+    private void Delay()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collisions++;
+        if ((collision.collider.CompareTag("Terrain") || collision.collider.CompareTag("Player")) && explodeOnTouch)
+            Explode();
+    }
+
+    private void Setup()
+    {
+        physics_mat = new PhysicMaterial();
+        physics_mat.bounciness = bounciness;
+        physics_mat.frictionCombine = PhysicMaterialCombine.Minimum;
+        physics_mat.bounceCombine = PhysicMaterialCombine.Maximum;
+
+        GetComponent<SphereCollider>().material = physics_mat;
+
+        rb.useGravity = useGravity;
+    }
+}
---
+++ Quickdesert/Assets/Scripts/BotsBehaviour/RocketBlueBots.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class RocketBlueBots : MonoBehaviour
+{
+    //bullet
+    public GameObject bullet;
+    public GameObject targetPlayer;
+
+    //bullet force
+    public float shootForce, upwardForce;
+
+    //Gun stats
+    public float timeBetweenShooting, spread, reloadTime, timeBetweenShots;
+    public int magazineSize, bulletPerTap;
+    public bool allowButtonHold;
+
+    int bulletsLeft, bulletsShot;
+
+    //bools
+    bool shooting, readyToShoot, reloading;
+
+    //Reference
+    //public Camera fpsCam;
+    public Transform attackPoint;
+
+    //Graphics
+    public GameObject muzzleFlash;
+    
+
+    Player player;
+    //bug fixing
+    public bool allowInvoke = true;
+
+    private void Awake()
+    {
+        bulletsLeft = magazineSize;
+        readyToShoot = true;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = GetComponent<BotGameplay>().player;
+
+        
+            MyInput();
+            //Set ammo display, if it exist          
+        
+    }
+    private void MyInput()
+    {
+        //Check if allowed to hold down button and take corresponding input
+        shooting = true;
+
+        //Reloading       
+        if (readyToShoot && shooting && !reloading && bulletsLeft <= 0)
+            Reload();
+
+        //Shooting
+        if (readyToShoot && shooting && !reloading && bulletsLeft > 0)
+        {
+            //Set bullets to 0
+            bulletsShot = 0;
+            Shot();
+        }
+        shooting = false;
+        Invoke("InvokeShooting", 2f);
+    }
+    void InvokeShooting()
+    {
+        shooting = true;
+    }
+    private void Shot()
+    {
+        readyToShoot = false;
+
+        //Find the exact hit position using a raycast
+        //Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        Ray ray = new Ray(attackPoint.position, targetPlayer.transform.position);
+        RaycastHit hit;
+
+        //check if ray hits something
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+            targetPoint = hit.point;
+        else
+            targetPoint = ray.GetPoint(100);
+
+        //Calculate direction from attackPoint to a targetPoint
+        Vector3 directionWithoutSpeed = targetPoint - attackPoint.position;
+
+        //Calculate spread
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        //Calculate new direction with spread
+        Vector3 directionWithSpread = directionWithoutSpeed + new Vector3(x, y, 0);
+
+        //Instantiate bullet/projectile
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position,
+            Quaternion.identity);
+
+        //Rotate bullet to shoot direction
+        currentBullet.transform.forward = directionWithSpread.normalized;
+
+        //Add forces to bullet
+        currentBullet.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * shootForce, ForceMode.Impulse);
+        //currentBullet.GetComponent<Rigidbody>().AddForce(fpsCam.transform.up.normalized * upwardForce, ForceMode.Impulse);
+
+        //Instantiate muzzle flash, if you have one
+        if (muzzleFlash != null)
+        {
+            GameObject flash = Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
+        }
+
+        bulletsLeft--;
+        bulletsShot++;
+
+        if (allowInvoke)
+        {
+            Invoke("ResetShot", timeBetweenShooting);
+            allowInvoke = false;
+        }
+
+        if (bulletsShot < bulletPerTap && bulletsLeft > 0)
+            Invoke("Shoot", timeBetweenShots);
+    }
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        allowInvoke = true;
+    }
+    private void Reload()
+    {
+        float reloadingTime = 0;
+     
+
+        reloading = true;
+        Invoke("ReloadFinished", reloadTime);
+    }
+    private void ReloadFinished()
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
+     
+    }
+
+}
